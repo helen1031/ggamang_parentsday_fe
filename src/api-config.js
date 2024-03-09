8fd4afff9e670c2e64424426f580a2cce1502115
helen1031 <32950301+helen1031@users.noreply.github.com>
2024-03-09 11:26:58 +0900

    update api-config

--- src/api-config.js
+++ src/api-config.js
@@ -4,6 +4,8 @@ const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
   backendHost = "http://localhost:8080";
+} else {
+  backendHost = "http://15.164.218.157:8080";
 }
 
 export const API_BASE_URL = `${backendHost}`;
