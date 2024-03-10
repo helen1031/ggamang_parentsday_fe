5d2a9e3db1afe59f3faad3f3619ad9905d71aa70
helen1031 <32950301+helen1031@users.noreply.github.com>
2024-03-10 19:38:27 +0900

    modify api-config.js

--- src/api-config.js
+++ src/api-config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if (hostname === "localhost") {
   backendHost = "http://localhost:8080";
 } else {
-  backendHost = "http://api.ggmanggarden.shop:8080";
+  backendHost = "http://api.ggamanggarden.shop:8080";
 }
 
 export const API_BASE_URL = `${backendHost}`;
