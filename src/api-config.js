492ecc4945cdeca4c5232df4e909d9a2e0456f0f
helen1031 <32950301+helen1031@users.noreply.github.com>
2024-03-10 19:24:02 +0900

    api-config update

--- src/api-config.js
+++ src/api-config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if (hostname === "localhost") {
   backendHost = "http://localhost:8080";
 } else {
-  backendHost = "http://15.164.218.157:8080";
+  backendHost = "http://api.ggmanggarden.shop:8080";
 }
 
 export const API_BASE_URL = `${backendHost}`;
